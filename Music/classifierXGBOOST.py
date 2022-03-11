04e56638b4e6e24f069103efe3b34735f7538ccb
Jasmijn-vH <98155591+Jasmijn-vH@users.noreply.github.com>
2022-03-11 10:04:29 +0100

    Feature preprocessing
    (mfcc, gfcc, thpcp, key/scale features)

--- Music/classifierXGBOOST.py
+++ Music/classifierXGBOOST.py
@@ -1,8 +1,12 @@
 import os
 import pandas
+import numpy as np
+import re
+
 from xgboost import XGBClassifier, plot_importance
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import OneHotEncoder
 
 
 
@@ -10,9 +14,37 @@ plt.style.use('ggplot')
 
 data = pandas.read_csv('./files/features_music_extractor.csv')
 
-# TO DO: How to handle categorical data?
-data_num = data.select_dtypes(include='number').iloc[:,1:]
-data_tot = pandas.concat([data['Contest'], data_num], axis=1)
+# Encode nominal variables using One Hot Encoding
+noms = ['tonal.chords_key', 'tonal.chords_scale', 
+        'tonal.key_edma.key', 'tonal.key_edma.scale',
+        'tonal.key_krumhansl.key', 'tonal.key_krumhansl.scale',
+        'tonal.key_temperley.key', 'tonal.key_temperley.scale']
+
+enc_data = pandas.get_dummies(data, columns=noms)
+
+# Process MFCC and GFCC lists
+for cc in ['lowlevel.gfcc.mean', 'lowlevel.mfcc.mean']:
+    for n in range(0,13):
+        enc_data[cc + '_' + str(n)] = np.nan
+    for row in range(0, len(enc_data.index)):
+        row_list = enc_data[cc][row]
+        row_list = row_list.replace("[", "").replace("]", "").replace("\n", " ")
+        row_list = row_list.split()
+        for n in range(0,13):
+            enc_data[cc + '_' + str(n)][row] = float(row_list[n])
+
+# Process THPCP
+for n in range(0,36):
+    enc_data['tonal.thpcp_' + str(n)] = np.nan
+for row in range(0, len(enc_data.index)):
+    row_list = enc_data['tonal.thpcp'][row]
+    row_list = row_list.replace("[", "").replace("]", "").replace("\n", " ")
+    row_list = row_list.split()
+    for n in range(0,36):
+        enc_data['tonal.thpcp_' + str(n)][row] = float(row_list[n])
+
+data_num = enc_data.select_dtypes(include='number').iloc[:,1:]
+data_tot = pandas.concat([enc_data['Contest'], data_num], axis=1)
 
 # Obtain overall feature importance
 X = data_tot.iloc[:,2:]
@@ -22,7 +54,7 @@ model = XGBClassifier()
 model.fit(X, y)
 
 importance_plot = plot_importance(model)
-importance_plot.figure.set_size_inches(10,50)
+importance_plot.figure.set_size_inches(10,60)
 if not os.path.exists('figures'):
     os.makedirs('figures')
 importance_plot.figure.savefig('./figures/importance_plot_total.png', bbox_inches='tight')
@@ -46,7 +78,8 @@ for year in [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021]:
 # # Test the classifier on 2022 songs
 # data_test = pandas.read_csv('./predictions2022/features_music_extractor2022.csv')
 
-# # TO DO: How to handle categorical data?
+# # TO DO: How to handle lists of floats in data?
+# #enc_data_test = pandas.get_dummies(data_test, columns=noms)
 # data_num_test = data_test.select_dtypes(include='number').iloc[:,1:]
 # data_tot_test = pandas.concat([data_test['Contest'], data_num_test], axis=1)
 
