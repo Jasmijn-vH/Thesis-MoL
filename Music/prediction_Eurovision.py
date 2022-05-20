c09e5ecddd87cb35266a70b975093ec5a217f028
Jasmijn-vH <98155591+Jasmijn-vH@users.noreply.github.com>
2022-05-20 15:27:34 +0200

    Remove ItSwe from rankings

--- Music/prediction_Eurovision.py
+++ Music/prediction_Eurovision.py
@@ -11,7 +11,7 @@ from xgboost import XGBRanker, XGBClassifier
 from sklearn import metrics
 from sklearn.model_selection import KFold, cross_val_predict
 
-from scipy.stats import rankdata, pearsonr
+from scipy.stats import rankdata, spearmanr
 
 
 def plot_confusion_matrix(y, y_pred, name):
@@ -44,18 +44,23 @@ plt.style.use('seaborn')
 data = pandas.read_csv('./files/features_music_extractor.csv')
 
 data_num = data.select_dtypes(include='number').iloc[:,1:]
-data_all = pandas.concat([data['Contest'], data['Place'], data_num], axis=1)
+data_all = pandas.concat([data['Contest'], data['Country'], data['Place'], data_num], axis=1)
 data_tot = data_all.loc[data_all['Year'] != 2022]
+
+data_tot_ESC  = data_tot.loc[data_tot['Contest'] == 'ESC']
+data_tot_ESC  = data_tot_ESC.loc[data_tot_ESC['Country'] != 'Italy']
+data_tot_ESC  = data_tot_ESC.loc[data_tot_ESC['Country'] != 'Sweden']
+
+data_tot_SRMF = data_tot.loc[data_tot['Contest'] != 'ESC']
 
 
 # Prediction by using a top 10 classifier
 for year in [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021]:
-    data_tot_year      = data_tot.loc[data_tot['Year'] == year]
-    data_tot_year_ESC  = data_tot_year.loc[data_tot_year['Contest'] == 'ESC']
-    data_tot_year_SRMF = data_tot_year.loc[data_tot_year['Contest'] != 'ESC']
+    data_tot_year_ESC  = data_tot_ESC.loc[data_tot_ESC['Year'] == year]
+    data_tot_year_SRMF = data_tot_SRMF.loc[data_tot_SRMF['Year'] == year]
 
-    X_year = data_tot_year_SRMF.iloc[:,3:]
-    y_year = data_tot_year_SRMF.iloc[:,1]
+    X_year = data_tot_year_SRMF.iloc[:,4:]
+    y_year = data_tot_year_SRMF.iloc[:,2]
     for i in y_year:
         if i in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']:
             y_year = y_year.replace(i, 'Yes')
@@ -72,13 +77,19 @@ for year in [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021]:
     evaluation_metrics(y_year, y_pred, str(year)+'_crossval')
 
     # Test model to predict top 10 of ESC
-    X_test = data_tot_year_ESC.iloc[:,3:]
-    y_test = data_tot_year_ESC.iloc[:,1]
+    X_test = data_tot_year_ESC.iloc[:,4:]
+    y_test = data_tot_year_ESC.iloc[:,2]
     for i in y_test:
-        if i in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']:
-            y_test = y_test.replace(i, 'Yes')
+        y_test = y_test.replace(i, float(i))
+    y_test = rankdata(y_test)
+
+    classes = []
+    for i in y_test:
+        if i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+            classes.append('Yes')
         else:
-            y_test = y_test.replace(i, 'No')
+            classes.append('No')
+
     predictions = model.predict(X_test)
     probs       = model.predict_proba(X_test)
 
@@ -92,23 +103,22 @@ for year in [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021]:
         else:
             predictions_10 = predictions_10 + ['No']
 
-    plot_confusion_matrix(y_test, predictions_10, str(year) + '_top10')
-    evaluation_metrics(y_test, predictions_10, str(year) + '_top10')
+    plot_confusion_matrix(classes, predictions_10, str(year) + '_top10')
+    evaluation_metrics(classes, predictions_10, str(year) + '_top10')
     print(predictions_10)
 
 
 # Prediction by ranker
 for year in [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021]:
-    data_tot_year      = data_tot.loc[data_tot['Year'] == year]
-    data_tot_year_ESC  = data_tot_year.loc[data_tot_year['Contest'] == 'ESC']
-    data_tot_year_SRMF = data_tot_year.loc[data_tot_year['Contest'] != 'ESC']
+    data_tot_year_ESC  = data_tot_ESC.loc[data_tot_ESC['Year'] == year]
+    data_tot_year_SRMF = data_tot_SRMF.loc[data_tot_SRMF['Year'] == year]
 
     data_tot_year_SRMF = data_tot_year_SRMF.dropna(subset='Place')
     data_tot_year_SRMF = data_tot_year_SRMF.replace(['E', 'QDSQ'], '20')
 
     # Train ranker
-    X = data_tot_year_SRMF.iloc[:,3:]
-    y = data_tot_year_SRMF.iloc[:,1]
+    X = data_tot_year_SRMF.iloc[:,4:]
+    y = data_tot_year_SRMF.iloc[:,2]
 
     group_sizes = data_tot_year_SRMF.groupby(['Contest']).size().to_frame('size')['size'].to_numpy()
     groups = [*group_sizes[1:], *group_sizes[:1]]
@@ -117,14 +127,16 @@ for year in [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021]:
     model.fit(X, y, group=groups)
 
     # Predict ranking of ESC
-    X_test = data_tot_year_ESC.iloc[:,3:]
-    y_test = data_tot_year_ESC.iloc[:,1]
+    X_test = data_tot_year_ESC.iloc[:,4:]
+    y_test = data_tot_year_ESC.iloc[:,2]
     for i in y_test:
         y_test = y_test.replace(i, float(i))
+    y_test = rankdata(y_test)
+
     predictions  = model.predict(X_test)
 
     ranks = rankdata(predictions)
-    print(ranks)
-    pears = pearsonr(ranks, y_test)
-    print(pears)
+    spearman = spearmanr(ranks, y_test)
+    print("\n" + str(year))
+    print(spearman)
     
